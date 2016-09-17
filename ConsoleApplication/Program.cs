312155fb51d50cd64b9e166e6aeb15a256191675
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-09-17 13:47:23 +0300

    Minimal console app

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+             Console.WriteLine(RuntimeInformation.OSDescription);
+        }
+    }
+}
